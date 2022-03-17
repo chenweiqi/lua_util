ce00d6e6ea06fd2a8bdb0788855164641c75b7f8
Weiqi Chen <251712045@qq.com>
2022-03-17 11:51:37 +0800

    fix table circular reference, optimize gc

--- table_util.lua
+++ table_util.lua
@@ -7,15 +7,20 @@ local version = _VERSION and tonumber(_VERSION:match("5.*"))
 function table_util.dirty_check(root, dirty_flag, gc_threshold)
 	dirty_flag = dirty_flag or "__dirty"
 	gc_threshold = gc_threshold or 100
+	assert(type(root) == "table", "not table")
 	assert(type(dirty_flag) == "string", "dirty_flag invalid")
 	assert(type(gc_threshold) == "number" and gc_threshold >= 0, "gc_threshold invalid")
 	assert(version>=5.2, "lua version >= 5.2")
+	
 	local dummy = {}
+	local g_set = {}
+	local threshold = 0
+	local is_dirty
+
 	local function assert_key(key)
 		assert(type(key) ~= "table", "not support table key")
 	end
-	local g_set = {}
-	local threshold = 0
+
 	local function check_alive(t, alive)
 		if type(t) ~= "table" or alive[t] then
 			return
@@ -25,47 +30,81 @@ function table_util.dirty_check(root, dirty_flag, gc_threshold)
 			check_alive(v, alive)
 		end
 	end
+
+	local function restore(t)
+		local mt = getmetatable(t)
+		if not mt then return end
+		assert(mt == dummy, "restore invalid metatable")
+		setmetatable(t, nil)
+		local gt = g_set[t]
+		local k,v
+		while true do
+			k, v = next(gt, k)
+			if not v then
+				break
+			end
+			if type(v) == "table" then
+				restore(v)
+			end
+			rawset(t, k, v)
+		end
+		g_set[t] = nil
+	end
+
 	local function gc_sweep()
+		if threshold == 0 then
+			return 0
+		end
 		threshold = 0
 		local alive = {}
 		check_alive(root, alive)
-		local all_cnt, del_cnt = 0,0
+		local cnt = 0
 		for t in pairs(g_set) do
-			all_cnt = all_cnt + 1
 			if not alive[t] then
-				g_set[t] = nil
-				del_cnt = del_cnt + 1
+				restore(t)
+				cnt = cnt + 1
 			end
 		end
-		return all_cnt, del_cnt
+		return cnt
 	end
+
+	local function count()
+		local cnt = 0
+		for _ in pairs(g_set) do
+			cnt = cnt + 1
+		end
+		return cnt
+	end
+
 	local function check_table(t)
 		if type(t) ~= "table" then
 			return
 		end
 		local mt = getmetatable(t)
 		assert(not mt or mt == dummy, "not support metatable")
-		local tv = g_set[t]
-		if not tv then
-			local tv = {}
-			for k, v in pairs(t) do
-				assert_key(k)
-				check_table(v)
-				tv[k] = v
-				rawset(t, k, nil) -- force trigger newindex
-			end
-			g_set[t] = tv
-		end
-		if not mt then
-			setmetatable(t, dummy)
-		end
-	end
-	dummy.__newindex = function(t, k, v)
-		assert_key(k)
-		if k ~= dirty_flag then
-			g_set[root][dirty_flag] = true
+		if g_set[t] then return end
+		local tv = {}
+		g_set[t] = tv
+		for k, v in pairs(t) do
+			assert_key(k)
 			check_table(v)
+			rawset(tv, k, v) -- move kv to g_set
+			rawset(t, k, nil) -- force trigger newindex
 		end
+		setmetatable(t, dummy)
+	end
+
+	dummy.__newindex = function(t, k, v)
+		if t == root and k == dirty_flag then
+			if v and v ~= true then
+				error(string.format("%s must be nil or true", dirty_flag))
+			end
+			is_dirty = v
+			return
+		end
+		is_dirty = true
+		assert_key(k)
+		check_table(v)
 		local old = g_set[t][k]
 		g_set[t][k] = v
 		if type(old) == "table" then
@@ -75,18 +114,37 @@ function table_util.dirty_check(root, dirty_flag, gc_threshold)
 			end
 		end
 	end
+
 	dummy.__index = function(t, k)
-		assert_key(k)
+		if t == root and k == dirty_flag then
+			return is_dirty
+		end
 		return g_set[t][k]
 	end
+
 	dummy.__pairs = function(t, k)
 		return next, g_set[t], k
 	end
+
 	dummy.__len = function(t)
 		return #g_set[t]
 	end
+
+	dummy.__call = function(t, option)
+		if t ~= root then
+			error('attempt to call a child table')
+		end
+		if option == "restore" then
+			return restore(t)
+		elseif option == "collect" then
+			return gc_sweep()
+		elseif option == "count" then
+			return count()
+		end
+		error(string.format('invalid option %s', option))
+	end
+
 	check_table(root)
-	return gc_sweep
 end
 
 -- table clone, avoid to modify table reference
