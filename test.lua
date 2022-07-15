7d70336836c8a189859921202737fe2338d32278
Weiqi Chen <251712045@qq.com>
2022-07-15 12:14:36 +0800

    add correct next

--- test.lua
+++ test.lua
@@ -1,12 +1,14 @@
 local table_util = require "table_util"
 
 local tb = {1,2,3, {}}
-local gc_sweep = table_util.dirty_check(tb, "__dirty", 100)
-print("foreach table tb")
+table_util.dirty_check(tb, "__dirty", 100)
+print(">> foreach table tb")
 for k,v in pairs(tb) do
 	print(k,v)
 end
 
+print()
+print(">> check dirty")
 print("table dirty 1", tb.__dirty)
 tb[3]=1
 print("table dirty 2", tb.__dirty)
@@ -35,6 +37,8 @@ tb.__dirty = nil
 t[3][2] = {}
 print("table dirty 7", tb.__dirty)
 
+print()
+print(">> check gc")
 -- check gc
 tb.__dirty = nil
 local tt = t[3][2]
@@ -53,9 +57,41 @@ tb.__dirty = nil
 
 print("table dirty 9", #tb)
 
-print("gc_sweep", gc_sweep())
+print("count", tb "count")
+print("collect", tb "collect")
+print("count", tb "count")
+
+
+print()
+print(">> fix next")
+
+print("next data", next(tb))
+
+local _next = next
+next = function(t, k)
+    local mt = getmetatable(t)
+    if mt and mt.__pairs then
+        local f, t1 = pairs(t)
+        return f(t1, k)
+    end
+    return _next(t, k)
+end
+print("next data", next(tb))
+
+
+
+print()
+print(">> check restore")
+for k,v in pairs(tb) do
+	print(k,v)
+end
+print("tb 4", tb[4])
+print("restore", tb "restore")
 
 for k,v in pairs(tb) do
 	print(k,v)
 end
-print(tb[4])
+for k,v in pairs(tb[4]) do
+	print(k,v)
+end
+print("tb 4", tb[4])
